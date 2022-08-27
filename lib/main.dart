b867744ff21222b0ca9b1e9fe493a73426d10cc5
Hallcoder <hallcoder25@gmail.com>
2022-08-27 17:04:36 +0200

    stateful and stateless widgets, and handling gesture actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: const Center(child: Text('Dice')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -14,19 +14,49 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
 
-class DicePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Row(
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeState(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
+@override
+Widget build(BuildContext context) {
+
+  return Center(
+    child: Row(
       children:  <Widget>[
         Expanded(
-          child: Image.asset('images/dice1.png')
+            child: TextButton(
+              onPressed: (){
+                this.changeState();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            )
         ),
         Expanded(
-          child: Image.asset('images/dice5.png'),
+          child: TextButton(
+            onPressed: (){
+             this.changeState();
+            },
+            child: Image.asset('images/dice$rightDiceNumber.png'),
+          ),
         ),
       ],
-    );
-  }
+    ),
+  );
 }
+}
+
+
